276f75cce31802ff1e33745700a089ad83d8f85f
Anjan Mondal <anjan.gk.mondal@gmail.com>
2025-06-14 00:51:34 +0530

    Build automatic Config object from generator.yml path

--- generator/config.py
+++ generator/config.py
@@ -1,31 +1,30 @@
+import enum
 import os
 from dataclasses import dataclass, field
-from typing import Generator, List, Optional
+from typing import List, Optional
 
 import yaml
 from dotenv import load_dotenv
 
 load_dotenv()
 
-config = None
 
+class FunctionType(enum.Enum):
+    PERIODIC = "periodic"
 
-@dataclass
-class DataGen:
-    "Class for keeping track of all the data generators"
-
-    datasets: dict = field(default_factory=dict)
-    configs: List["Config"] = field(default_factory=list)
-
-    @property
-    def num_configs(self):
-        return len(self.config[self.datasets])
+    @classmethod
+    def from_str(cls, name: str):
+        name = name.lower()
+        for member in cls:
+            if member.value.lower() == name:
+                return member
+        raise ValueError(f"Invalid generator type: {name}")
 
 
 @dataclass
 class Config:
     name: str
-    type: str
+    type: FunctionType
     size: Optional[int] = 100
     start: Optional[int] = 0
     stop: Optional[int] = 1
@@ -33,16 +32,33 @@ class Config:
     noise_scale: Optional[float] = 0.1
 
 
-def get_config() -> Generator["Config"]:
-    global config
-    path = os.getenv("CONFIG_PATH")
-    if not config:
-        with open(path, "r") as file:
-            config = yaml.safe_load(file)
-    return config
+@dataclass
+class DataGen:
+    "Class for keeping track of all the data generators"
+
+    configs: List[Config] = field(default_factory=list)
+
+    @classmethod
+    def build(cls, config_path: Optional[str] = None):
+        if config_path is None:
+            config_path = os.getenv("CONFIG_PATH")
+
+        with open(config_path, "r") as file:
+            configs = yaml.safe_load(file)
+
+        return cls(
+            configs=[
+                Config(**{**cfg, "type": FunctionType.from_str(cfg["type"])})
+                for cfg in configs["datasets"]
+            ]
+        )
+
+    @property
+    def num_configs(self):
+        return len(self.configs)
 
 
 if __name__ == "__main__":
-    config = get_config()
-    config = DataGen(config)
-    print(config)
+    data_gen = DataGen.build()
+    print(data_gen.configs[0].type)
+    print(data_gen.num_configs)
